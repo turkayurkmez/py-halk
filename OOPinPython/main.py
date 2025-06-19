8fe214b442031f02180b1848bde6ed502cbb4222
türkay ürkmez <turkay.urkmez@outlook.com>
2025-06-19 12:18:14 +0300

    Refactor Product class to enhance initialization and add price history tracking; improve stock management and comparison methods

--- OOPinPython/main.py
+++ OOPinPython/main.py
@@ -2,11 +2,11 @@ import product
 
 
 if __name__ == "__main__":
-    laptop = product.Product()
-    laptop.name ="Dell XPS"
-    laptop.price = 45000
-    laptop.stock = 150
-    laptop.category = "Elektronik"
+    laptop = product.Product("Dell XPS",45000,0,"Elektronik")
+    # laptop.name ="Dell XPS"
+    # laptop.price = 45000
+    # laptop.stock = 150
+    # laptop.category = "Elektronik"
 
  
     
@@ -15,3 +15,65 @@ if __name__ == "__main__":
     print(laptop.check_stock())
     
     product.Product.test()
+    
+    print(laptop)
+    print("Repr ", repr(laptop))
+    print("Len ",len(laptop))
+
+    if laptop:
+        print("Stokta var")
+    else:
+        print("stokta yok")
+
+    notebook = product.Product("Lenovo",35000,0,"Elektronik")
+
+    if laptop == notebook:
+        print("İki nesne eşit")
+    else:
+        print("Farklı nesneler")
+
+    if notebook < laptop:
+        print(notebook, "bu ürün daha ucuz")
+
+    if laptop>notebook:
+        print("laptop pahalı...")
+    
+    output = max(laptop,notebook)
+    print(output)
+
+    total = laptop + notebook
+    print("toplam:",total)
+
+    laptop._features.append("32 GB Ram")
+    laptop._features.append("1 TB SSD")
+
+    if "32 GB Ram" in laptop:
+        print("Bu özellik var")
+    else:
+        print("Bu özellik yok")
+
+    products = [laptop,notebook]
+    sorted = sorted(products)
+
+    print(sorted)
+
+    name = input("Ürün adı: ")
+    # if len(name) < 3 :
+    #     raise ValueError("Ürün adı en az üç harfli olmalı")
+    
+    # laptop.name = name
+
+    laptop.name = name
+    print(laptop.name)
+
+    laptop.price = 55000
+
+    print("güncel fiyat: ",laptop.price)
+
+
+
+
+    # value = str(laptop)
+    # print(value)
+
+    #laptop.test()
--- OOPinPython/product.py
+++ OOPinPython/product.py
@@ -1,25 +1,113 @@
+import datetime
+
+
 class Product:
     """ Bir E-Ticaret uygulaması için Ürün sınıfı """
     
-    name: str = None
-    price: float = 1.0
-    stock: int = 1
+    # name: str = None
+    # _price: float = 1.0
+    # _stock: int = 1
+    _features:list = []
+
+    def __init__(self, name, _price,_stock,_category):
+        self._name =name
+        self._price = _price
+        self._stock = _stock
+        self._category = _category
+        self._price_history = [_price]
+        self._updated_date = datetime.datetime.now()
+        print("instance oluşturuldu")
+
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name:str):
+        if not isinstance(new_name,str) or len(new_name)< 3:
+            raise ValueError("Ürün adı en az 3 harfli olmalı")
+        
+        old_name = self._name
+        self._name = new_name
+        print(f"{old_name} olan isim {new_name} olarak güncdellendi")
+        self._updated_date = datetime.datetime.now()
+        print(self._updated_date)
+
+    
+    @property
+    def price(self):
+        return self._price
+    
+    @price.setter
+    def price(self, new_price):
+        if not isinstance(new_price, (int,float)) or new_price <= 0:
+            raise ValueError("fiyat pozitif sayı olmalı")
+        
+        old_price = self._price
+        self._price = new_price
+        self._price_history.append(new_price)
+
+        change_rate = ((new_price-old_price)/old_price) * 100
+        if change_rate > 0:
+            print(f"{self._name} fiyatı, %{change_rate} artarak {new_price} oldu ")
+        else:
+            print(f"{self._name} fiyatı, %{change_rate} azalarak {new_price} oldu ")
+
+        
+        print("Ürün fiyat geçmişi: ",self._price_history)
+
+        self._updated_date = datetime.datetime.now()
+
+
+
+    
+    def __str__(self):
+        return f"{self._name} isimli ürünün fiyatı {self._price} TL'dir ve stokta {self._stock} adet vardır. Ayrıca kategorisi {self._category} olarak belirtilmiştir"
+
+    def __repr__(self):
+        return f"Product(name:{self.name}, price:{self._price}, stock:{self._stock}, category:{self._category})"    
+    
+    def __len__(self):
+        return self._stock
+    
+    def __bool__(self):
+        return self._stock > 0
+    
+    def __eq__(self, value):
+        return self._name == value.name and self._price == value._price
+    
+    def __lt__(self, value):
+        return self._price < value._price
+    
+    def __gt__(self,value):
+        return self._price > value._price
+    
+    def __add__(self, value):
+        return self._price + value._price
+    
+    def __contains__(self,item):
+        return item in self._features
+
+    
 
     def show_details(self):
-        return f"{self.name} - {self.price} - {self.stock}"
+        return f"{self._name} - {self._price} - {self._stock}"
     
     def update_price(self, new_price):
-        old_price = self.price
-        self.price = new_price
-        print(f"{self.name} isimli ürünün yeni fiyatı {self.price} eski fiyatı ise {old_price} idi")
+        old_price = self._price
+        self._price = new_price
+        print(f"{self._name} isimli ürünün yeni fiyatı {self._price} eski fiyatı ise {old_price} idi")
     
     def check_stock(self):
-        if self.stock > 10:
+        if self._stock > 10:
             return "stokta var"
-        elif self.stock > 0:
+        elif self._stock > 0:
             return "stok kritik"
         else:
             return "Stokta yok"
     
     def test():
         print("Bu fonksiyon, sadece class içinden çağrılabilir")
+
+    
